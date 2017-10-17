e1cb727a924785db4cadfc19cbf17cd8f9b5952a
Leblond Valentin <21609038@n304l-403019.etu.info.unicaen.fr>
2017-10-17 12:25:38 +0200

    initial commit

---
+++ Main.java
@@ -0,0 +1,57 @@
+
+package edt_java;
+
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+
+class Main {
+
+  public static void main (String[] args) {
+
+    /*
+    Activity options = new Activity ("Manger des Chocapic", 70);
+    Activity ip = new Activity ("Faire une game de LOL", 45);
+
+    PrecedenceConstraint contrainte = new PrecedenceConstraint (options, ip);
+
+    GregorianCalendar date1 = new GregorianCalendar(2008,0,1,23,0);
+    GregorianCalendar date2 = new GregorianCalendar(2008,0,2,0,0);
+    GregorianCalendar date3 = new GregorianCalendar(2017,6,24,15,0);
+
+    // Satisfaite
+    if (contrainte.isSatisfied(date1, date3) ) {
+        System.out.println("Satisfaite");
+    } else {
+        System.out.println("Non Satisfaite");
+    }
+
+    // Non Satisfaite
+    if (contrainte.isSatisfied(date1, date2) ) {
+      System.out.println("Satisfaite");
+    } else {
+      System.out.println("Non Satisfaite");
+    }
+
+    // Non Satisfaite
+    if ( contrainte.isSatisfied(date1, date1) ) {
+      System.out.println("Satisfaite");
+    } else {
+      System.out.println("Non Satisfaite");
+    }
+
+    Activity options = new Activity ("Manger des Chocapic", 70);
+    Activity ip = new Activity ("Faire une game de LOL", 45);
+
+    PrecedenceConstraint contrainte = new PrecedenceConstraint (options, ip);
+
+    GregorianCalendar date1 = new GregorianCalendar(2008,0,1,23,0);
+    GregorianCalendar date2 = new GregorianCalendar(2008,0,2,0,0);
+    GregorianCalendar date3 = new GregorianCalendar(2017,6,24,15,0);
+
+    PrecedenceConstraintWithDuration contrainte1 = new PrecedenceConstraintWithDuration(options,ip,0,70);
+
+    System.out.println("" + contrainte1.isSatisfied(date1,date2));
+    */
+
+  }
+}
