6736988b1648541205136844dcb7b75bc62812af
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2017-11-17 16:40:39 +0100

    ScheduleReader

--- Main.java
+++ Main.java
@@ -1,134 +1,17 @@
 
 package edt_java;
 
-import java.util.GregorianCalendar;
-import java.util.ArrayList;
-import java.util.HashMap;
+import scheduleio.*;
 import java.util.Map;
+import java.io.IOException;
 
 class Main {
 
-  public static void main (String[] args) {
+  public static void main (String[] args) throws IOException {
 
     System.out.println("______________________________\n");
-
-    /*
-    Activity act1 = new Activity ("Manger des Chocapic", 70);
-    Activity act2 = new Activity ("Faire une game de LOL", 45);
-
-    PrecedenceConstraint contrainte = new PrecedenceConstraint (act1, act2);
-
-    GregorianCalendar date1 = new GregorianCalendar(2008,0,1,23,0);
-    GregorianCalendar date2 = new GregorianCalendar(2008,0,2,0,0);
-    GregorianCalendar date3 = new GregorianCalendar(2017,6,24,15,0);
-
-    // Satisfaite
-    if (contrainte.isSatisfied(date1, date3) ) {
-        System.out.println("Satisfaite");
-    } else {
-        System.out.println("Non Satisfaite");
-    }
-
-    // Non Satisfaite
-    if (contrainte.isSatisfied(date1, date2) ) {
-      System.out.println("Satisfaite");
-    } else {
-      System.out.println("Non Satisfaite");
-    }
-
-    // Non Satisfaite
-    if ( contrainte.isSatisfied(date1, date1) ) {
-      System.out.println("Satisfaite");
-    } else {
-      System.out.println("Non Satisfaite");
-    }
-    */
-
-    /*
-    Activity act1 = new Activity ("Manger des Chocapic", 75);
-    Activity act2 = new Activity ("Faire une game de LOL", 120);
-    Activity act3 = new Activity ("Regarder Koh-Lanta", 240);
-    Activity act4 = new Activity ("Réviser", 60);
-
-    ArrayList<Activity> activities = new ArrayList<> ();
-    activities.add(act1);
-    activities.add(act2);
-    activities.add(act3);
-    activities.add(act4);
-
-    PrecedenceConstraint contrainte1 = new PrecedenceConstraint (act1, act2);
-    PrecedenceConstraint contrainte2 = new PrecedenceConstraint (act2, act3);
-    PrecedenceConstraint contrainte3 = new PrecedenceConstraint (act3, act4);
-
-    ArrayList<PrecedenceConstraint> list_contraintes = new ArrayList<> ();
-    list_contraintes.add(contrainte1);
-    list_contraintes.add(contrainte2);
-    list_contraintes.add(contrainte3);
-
-    GregorianCalendar date1 = new GregorianCalendar(2008,0,1,23,0);
-    GregorianCalendar date2 = new GregorianCalendar(2008,0,3,1,10);
-    GregorianCalendar date3 = new GregorianCalendar(2017,4,16,4,0);
-    GregorianCalendar date4 = new GregorianCalendar(2017,4,16,8,0);
-
-    //PrecedenceConstraintWithDuration contrainte1 = new PrecedenceConstraintWithDuration(act1,act2,40,50);
-
-    Schedule edt = new Schedule();
-
-    //edt.schedule(act1,date1);
-    //edt.schedule(act2,date2);
-    //edt.schedule(act3,date3);
-    //edt.schedule(act4,date4);
-
-    edt.computeSchedule(activities,list_contraintes);
-    System.out.print(edt.toString());
-    */
-
-    Activity act1 = new Activity ("Manger des Chocapic", 60);
-    Activity act2 = new Activity ("Faire une game de LOL", 120);
-    Activity act3 = new Activity ("Regarder Koh-Lanta", 240);
-    Activity act4 = new Activity ("Réviser", 60);
-
-    ArrayList<Activity> activities = new ArrayList<> ();
-    activities.add(act1);
-    activities.add(act2);
-    activities.add(act3);
-    activities.add(act4);
-
-    PrecedenceConstraint contrainte1 = new PrecedenceConstraint (act1, act2);
-    PrecedenceConstraintWithDuration contrainte2 = new PrecedenceConstraintWithDuration (act2, act3, 10, 3600*24*365*12);
-    MeetConstraint contrainte3 = new MeetConstraint (act3, act4);
-
-    GregorianCalendar date1 = new GregorianCalendar(2017,4,16,5,0);
-    GregorianCalendar date2 = new GregorianCalendar(2017,4,16,7,10);
-    GregorianCalendar date3 = new GregorianCalendar(2017,4,16,10,0);
-    GregorianCalendar date4 = new GregorianCalendar(2017,4,16,14,0);
-
-    ArrayList<BinaryConstraint> list_contraintes = new ArrayList<> ();
-    list_contraintes.add(contrainte1);
-    list_contraintes.add(contrainte2);
-    list_contraintes.add(contrainte3);
-
-    // test de isSatisfied de MeetConstraint //
-    //System.out.println(contrainte1.isSatisfied(date1,date2));
-
-    Schedule edt = new Schedule();
-
-    edt.computeSchedule(activities,list_contraintes);
-    if (contrainte1.isSatisfied(date1, date2) && contrainte2.isSatisfied(date2,date3) && contrainte3.isSatisfied(date3,date4)) {
-      System.out.print(edt.toString());
-    } else {
-      System.out.println("Nope");
-    }
-
-    ArrayList<Activity> activities1 = new ArrayList<> ();
-    activities1.add(act1);
-    activities1.add(act2);
-    activities1.add(act3);
-    activities1.add(act4);
-
-    MaxSpanConstraint test = new MaxSpanConstraint(activities1,60*9);
-    boolean test2 = test.isSatisfied(edt);
-    System.out.println(test2);
+		ScheduleReader test = new ScheduleReader();
+		Map<String,Activity> test2 = test.readActivities("edt_java/activities.txt");
     System.out.println("______________________________\n");
   }
 }
---
+++ ScheduleReader.java
@@ -0,0 +1,36 @@
+package edt_java;
+
+import java.util.Map;
+import java.util.HashMap;
+import scheduleio.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ScheduleReader {
+
+	public ScheduleReader() {
+	}
+
+	public Map<String, Activity> readActivities(String filename) throws IOException {
+		BufferedReader fileReader = new BufferedReader(new FileReader(filename));
+		IdStringStringReader activityReader = new IdStringStringReader(fileReader, "=", "_lasting_");
+		Map<String, Activity> activityMap = new HashMap<>();
+		for (Map.Entry<String, OrderedPair<String, String>> strings: activityReader.readAll().entrySet()) {
+			String id = strings.getKey();
+			OrderedPair<String, String> activityStrings = strings.getValue();
+			String description = activityStrings.getFirst();
+			int duration = Integer.parseInt(activityStrings.getSecond());
+			activityMap.put(id,new Activity(description,duration));
+		}
+		return activityMap;
+	}
+
+	public ArrayList<PrecedenceConstraint> buildPrecedenceCollection (Map<String,Activity> activities, String fileConstraintsName) throws IOException {
+		BufferedReader fileReader = new BufferedReader(new FileReader(filename));
+		IdStringStringReader activityReader = new IdStringStringReader(fileReader, activities, "_before_"); //TODO
+		for (Map.Entry<String,Activity> elt : activities.entrySet()) {
+
+    }
+	}
+}
